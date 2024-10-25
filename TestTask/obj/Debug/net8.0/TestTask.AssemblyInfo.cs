2d2d19de5526f53134e4aad955c3d7c39c958178
Szymon <Szymon@SZYMON>
2024-10-25 16:33:57 +0200

    Changes

--- TestTask/obj/Debug/net8.0/TestTask.AssemblyInfo.cs
+++ TestTask/obj/Debug/net8.0/TestTask.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestTask")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+673db95628d7f70798379de12c8bdd03080c9d45")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestTask")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestTask")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
